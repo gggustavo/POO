08718d7fe4449b072c91684b2901b6d268a3a54f
gggustavo <ggustavo@icloud.com>
2016-04-12 21:42:36 -0300

    add class person

---
+++ ToolKit/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolKit
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string lastName { get; set; }
+        public int Age { get; set; }
+    }
+}
--- ToolKit/Program.cs
+++ ToolKit/Program.cs
@@ -10,6 +10,15 @@ namespace ToolKit
     {
         static void Main(string[] args)
         {
+            var person = new Person()
+            {
+              Name = "POO",
+              lastName = "UAI",
+              Age = 30
+            };
+
+            Console.WriteLine(string.Format("Name:{0}\nAge:{1}", person.Name, person.Age));
+            Console.ReadKey();
         }
     }
 }
