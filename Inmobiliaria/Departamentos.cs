1e0a64fb88e7669ac615c8c21a51e9a5e5a00f76
gggustavo <ggustavo@icloud.com>
2017-04-18 21:08:58 -0300

    change

---
+++ Inmobiliaria/Departamentos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inmobiliaria
+{
+    public partial class Departamentos : Form
+    {
+        private Inmobiliaria _inmobiliaria;
+
+        public Departamentos(Inmobiliaria inmobiliaria)
+        {
+            InitializeComponent();
+
+            _inmobiliaria = inmobiliaria;
+        }
+
+        private void Departamentos_Load(object sender, EventArgs e)
+        {
+            dgDepartamentos.DataSource = _inmobiliaria.ListarDepartamentos();
+        }
+    }
+}
