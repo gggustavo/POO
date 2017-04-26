55d74720ce843b0e1c7e724dc90c17dc4689e963
gggustavo <ggustavo@icloud.com>
2017-04-25 21:29:57 -0300

    add file

---
+++ Repuesto/CasaRepuestos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repuesto
+{
+    public class CasaRepuestos
+    {
+        public List<Articulo> Articulos { get; set; }
+
+        public CasaRepuestos()
+        {
+            Articulos = new List<Articulo>();
+        }
+
+        public void AgregarArticulo(Articulo articulo) 
+        {
+            Articulos.Add(articulo);
+        }
+
+        public void EliminarArticulo(int nroSerie)
+        {
+            var articulo = Articulos.FirstOrDefault(_ => _.NumeroSerie == nroSerie);            
+            Articulos.Remove(articulo);
+        }
+
+        public void EliminarArticulo(Articulo articulo)
+        {
+            Articulos.Remove(articulo);
+        }
+
+        public List<Articulo> ListarArticulos()
+        {
+            return Articulos.ToList();
+        }
+
+    }
+}
